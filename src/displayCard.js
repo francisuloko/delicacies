ad7061ac303d41dbdf73223755fc50df0a57be5f
Francis Uloko <francisuloko@yahoo.com>
2021-08-11 15:39:04 +0100

    Update basic structure

---
+++ src/displayCard.js
@@ -0,0 +1,17 @@
+export default function displayCard(meals){
+  const grid = document.getElementById('meals-grid');
+  grid.innerHTML = '';
+  meals.forEach (meal => {
+    const card = createElement('div')
+    card.setAttribute('class', 'col-sm-4 themed-grid-col')
+    card.innerHTML = `
+      <img src="${meal}" alt="sample image">
+      <div class="d-flex justify-content-between">
+        <h3 class="w-100">${meal}</h3>
+        <button type="button" id="like">Like</button>
+      </div>
+      <p class="counter">Counter</p>
+      <button type="button" id="comment">Comment</button>`;
+    grid.appendChild(card)
+  });
+}
