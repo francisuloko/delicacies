ce0c8f9ff08d1f42de3bc691f0755b0cb9ca0165
francisuloko <francisuloko@yahoo.com>
2022-04-21 20:37:03 +0100

    Clean up likes code

--- src/like.js
+++ src/like.js
@@ -1,19 +1,15 @@
-import API from './api.js';
+import API from "./api.js";
 
 const api = new API();
-const likeButtons = document.querySelectorAll('.like-button');
 export default class Like {
   create(e) {
     const meal = e.target;
-    api.post(api.urls.likes, { item_id: meal.id })
+    let likesCount = document.getElementById(`meal-${e.target.id}-likes`);
+    api
+      .post(api.urls.likes, { item_id: meal.id })
       .then((saved) => saved)
       .catch((err) => err);
-    const elem = document.getElementById(meal.id);
-    const likes = Number(elem.parentNode.nextSibling.innerHTML) + 1;
-    elem.parentNode.nextSibling.innerHTML = likes;
-    likeButtons.forEach((button) => {
-      button.addEventListener('click', this.create);
-    });
+    likesCount.innerHTML = +likesCount.innerHTML + 1;
   }
 
   get() {
@@ -22,7 +18,7 @@ export default class Like {
 
   show(res) {
     this.res = res;
-    const items = document.querySelectorAll('.likes');
+    const items = document.querySelectorAll(".likes");
     for (let i = 0; i < items.length; i += 1) {
       items[i].innerHTML = res[i].likes;
     }
